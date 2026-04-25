4e36efc653deb069e45263b225c9bc8f55524a1a
hbin0529 <small052@naver.com>
2026-04-26 00:46:48 +0900

    db_connection script create

--- scripts/db_connection.py
+++ scripts/db_connection.py
@@ -1,1 +1,18 @@
 import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+def get_engine():
+    load_dotenv()
+
+    user = os.getenv("POSTGRES_USER")
+    password = os.getenv("POSTGRES_PASSWORD")
+    database = os.getenv("POSTGRES_DB")
+    host = os.getenv("POSTGRES_HOST", "localhost")
+    port = os.getenv("POSTGRES_PORT", "5432")
+
+    if not all([user, password, database]):
+        raise ValueError("Database environment variables are missing")
+    
+    db_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+    return create_engine(db_url)
